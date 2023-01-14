dbaec9f595a9d413c08c4293e2efce08e6fe04ac
asdfwowza <115117306+Woahai321@users.noreply.github.com>
2023-01-14 06:15:23 +0000

    Update Error.js

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,15 +2,23 @@ import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
-  // console.error(error, error.statusText);
 
   return (
     <div id="error-page">
       <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <p>Sorry, an unexpected error has occurred. The error details are:</p>
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
+      <p>
+        <b>Error code:</b> {error.status}
+      </p>
+      <p>
+        <b>Error message:</b> {error.message}
+      </p>
+      <p>Try going back to the previous page or refreshing the current page. If the problem persists, please contact our support team for assistance.</p>
+      <button onClick={()=>{window.history.back()}}>Go Back</button>
+      <button onClick={()=>{window.location.reload()}}>Refresh</button>
     </div>
   );
 }
